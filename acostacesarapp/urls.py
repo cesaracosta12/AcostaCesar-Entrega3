f9b96c93ca7e10c007296f494af9f29308d0fef1
CesarAcosta <ocesaracosta@gmail.com>
2024-06-11 17:07:15 -0300

    disenio agregado

--- acostacesarapp/urls.py
+++ acostacesarapp/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from acostacesarapp import views
 urlpatterns = [
     path('', views.inicio, name="inicio"),
+    path('contacto', views.contacto, name="contacto"),
+    path('nuevo', views.cargarnuevo, name="cargarnuevo"),
+    path('listado',views.listado, name="listado"),
+    
 ]
--- acostacesarapp/views.py
+++ acostacesarapp/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def inicio(request):
     return render(request,'acostacesarapp/index.html')
+
+def contacto(request):
+    return render(request,'acostacesarapp/contacto.html')
+
+def cargarnuevo(request):
+    return render(request,'acostacesarapp/cargarnuevo.html')
+    
+def listado(request):
+    return render(request,'acostacesarapp/listado.html')
+    
