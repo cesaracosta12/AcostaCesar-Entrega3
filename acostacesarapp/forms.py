a5cbc8b6cf261af9663370b515240d7710705500
Cesar <ocesaracosta@gmail.com>
2024-06-13 11:27:15 -0300

    messi

--- acostacesarapp/forms.py
+++ acostacesarapp/forms.py
@@ -4,7 +4,8 @@ class CrearPersonaFormulario(forms.Form):
     nombre = forms.CharField(max_length=40,required=True)
     apellido = forms.CharField(max_length=40,required=True)
     dni= forms.IntegerField(required=True)
-    edad= forms.IntegerField(required=False)
+    edad= forms.IntegerField()
     correo = forms.EmailField(max_length=20,required=True)
     direccion =  forms.CharField(max_length=50)
     especialidad = forms.CharField(max_length=40,required=True)
+    estado = True
--- acostacesarapp/views.py
+++ acostacesarapp/views.py
@@ -9,22 +9,18 @@ def contacto(request):
     return render(request,'acostacesarapp/contacto.html')
 
 def cargarnuevo(request):
+    
+    formulario = CrearPersonaFormulario()
     if request.method == 'POST':
         formulario = CrearPersonaFormulario(request.POST)
+        print(request.POST)
+        print('is valid?',formulario.is_valid())
         if formulario.is_valid():
-                datos = formulario.cleaned_data
-                print('datos: ',datos)
-                persona = Persona(nombre = datos.get("nombre"),
-                                  apellido = datos.get('apellido'),
-                                  correo = datos.get('correo'),
-                                  dni = datos.get("dni"),
-                                  edad = datos.get("edad"),
-                                  direccion = datos.get("direccion"),
-                                  especialidad = datos.get("especialidad"))
-                persona.save()
-                return redirect('listado')
-    else:
-        formulario = CrearPersonaFormulario()  
+            datos = formulario.cleaned_data
+            print('datos: ',datos)
+            persona = Persona(nombre = datos.get("nombre"), apellido = datos.get('apellido'),correo = datos.get('correo'),dni = datos.get("dni"),edad = datos.get("edad"),direccion = datos.get("direccion"),especialidad = datos.get("especialidad"))
+            persona.save()
+            return redirect('listado') 
     return render(request,'acostacesarapp/cargarnuevo.html', {'formulario':formulario})
     
 def listado(request):
